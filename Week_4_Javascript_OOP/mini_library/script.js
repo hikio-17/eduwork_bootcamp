c0a1371c3013eb39b4a5cc1c77d14e1829c0370b
hikio-17 <fajri010217@gmail.com>
2022-01-11 04:30:01 -0800

    initial commit

--- Week_4_Javascript_OOP/mini_library/script.js
+++
@@ -1,7 +0,0 @@
-new gridjs.Grid({
-      columns: ["Nama", "Email", "No. Telepone"],
-      data: [
-        ["Fajri Muhammad Tio", "fajritio376@gmail.com", "(+62) 822 3788 8697"],
-        ["Hikma Nelda", "hikmanelda07@gmail.com", "(+62) 822 7765 4667"]
-      ]
-    }).render(document.getElementById("table-gridjs"));
