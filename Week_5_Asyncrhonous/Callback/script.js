1931117bd5b668b26a96ec70bbc5c84bf37c2d81
hikio-17 <fajri010217@gmail.com>
2022-01-26 06:15:54 -0800

    update

--- Week_5_Asyncrhonous/Callback/script.js
+++ Week_5_Asyncrhonous/Callback/script.js
@@ -1,36 +1,39 @@
+function getData(url, cb) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      return cb(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
+}
+
 const table = document.getElementById("data");
 table.innerHTML = loading();
-const xhr = new XMLHttpRequest();
-
-const getData = (url, cb)=> {
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            document.getElementById('data').innerHTML = '';
-            return cb(JSON.parse(xhr.responseText));
-        }
-    };
-    xhr.open('GET', url);
-    xhr.send();
-}
-
-const data = ()=> {
-    getData('https://jsonplaceholder.typicode.com/users', (data)=> {
-        data.forEach( (element) => {
-            document.getElementById('data').innerHTML += `<tr> 
-                                                            <th>${element.id}</th> 
-                                                            <td>${element.name}</td>
-                                                            <td>${element.username}</td> 
-                                                            <td>${element.email}</td> 
-                                                            <td>${element.address.street}, ${element.address.suite}, ${element.address.city}</td> <td>${element.company.name}</td> 
-                                                            </tr>`;
-        });
-        });
-}
-
-data();
-
+// fungsi loading
 function loading() {
-  return `<tr>
-    <td colspan="6" class="text-center">Sabar Lagi Loading...</td>
+  return `<tr> 
+  <td colspan='6' class='text-center'> Proses Loading .. </td>
   </tr>`;
 }
+
+const data = getData("https://jsonplaceholder.typicode.com/users", function (data) {
+  // clear loading
+  document.getElementById("data").innerHTML = "";
+  data.forEach((data) => {
+    // menampilkan data table
+    document.getElementById("data").innerHTML += `<tr>
+    <td>${data.id}</td>
+    <td>${data.name}</td>
+    <td>${data.username}</td>
+    <td>${data.email}</td>
+    <td>
+    ${data.address.street}
+    ${data.address.suite}
+    ${data.address.city}
+    </td>
+    <td>${data.company.name}</td>
+    </tr>`;
+  });
+});
